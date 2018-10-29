927cd31d5ad4e735ca381d15726e0b546c311bf9
limd0024 <32812631+limd0024@users.noreply.github.com>
2018-10-29 19:52:21 +0800

    Add files via upload

---
+++ src/com/CE4062Assignment/Test/Main.java
@@ -0,0 +1,12 @@
+package com.CE4062Assignment.Test;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Arguments! (" + Arrays.toString(args) + ")");
+	}
+
+}
